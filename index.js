e9f76a908c37accf3c0f87cb809cd047ae15b803
Katie Strauss <straussk@my.lanecc.edu>
2019-04-08 14:38:47 -0700

    maybe this will work this time?

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
     Create 3 global variables, isRunning, timer and timerTime.
     Initialize them to false, null and 0 respectively.
 */
+var isRunning = false;
+var timer = null;
+var timerTime = 0;
 
 function init()
 {
     // Put the element on the page with an id of start in a variable
     // Do the same for the stop button and the reset button
 
+    var startBtn = document.getElementById("start");
+    var stopBtn = document.getElementById("stop");
+    var resetBtn = document.getElementById("reset");
 
     // Add an onclick handler to each of the buttons
     // Use the functions startTimer, stopTimer and resetTimer 
 
+    startBtn.onclick = startTimer;
+    stopBtn.onclick = stopTimer;
+    resetBtn.onclick = resetTimer;
 }
 
 
@@ -20,14 +29,26 @@ function startTimer() {
     // call the function incrementTimer every second
     // save the timer in a the timer variable
 
+    if (!isRunning) {
+        isRunning = true;
+        timer = setInterval("incrementTimer()", 1000);
+    }
 }
 
 function incrementTimer() {
     // increment the timerTime
     // calculate the number of minutes and seconds
 
+    timerTime++;
+
     // write the minutes and seconds to the elements on the page
     // use the function pad to make sure there's a leading 0 if necessary
+
+    var min = Math.floor(timerTime / 60);
+    var sec = timerTime % 60;
+
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
 }
 
 function pad(number) {
@@ -49,3 +70,4 @@ function resetTimer() {
 }
 
 // When the page has finished loading, call the function init
+window.onload = init;
