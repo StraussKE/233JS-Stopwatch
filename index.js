46a33fc39792f5e0d5a6190f61b7fb69afa7716a
Katie Strauss <straussk@my.lanecc.edu>
2019-04-09 12:56:23 -0700

    this, this, this

--- index.js
+++ index.js
@@ -2,16 +2,24 @@
     Create 3 global variables, isRunning, timer and timerTime.
     Initialize them to false, null and 0 respectively.
 */
+var isRunning = false;
+var timer = null;
+var timerTime = 0;
 
-function init()
-{
+function init() {
     // Put the element on the page with an id of start in a variable
     // Do the same for the stop button and the reset button
 
+    var startBtn = document.getElementById("start");
+    var stopBtn = document.getElementById("stop");
+    var resetBtn = document.getElementById("reset");
 
     // Add an onclick handler to each of the buttons
     // Use the functions startTimer, stopTimer and resetTimer 
 
+    startBtn.onclick = startTimer;
+    stopBtn.onclick = stopTimer;
+    resetBtn.onclick = resetTimer;
 }
 
 
@@ -20,32 +28,53 @@ function startTimer() {
     // call the function incrementTimer every second
     // save the timer in a the timer variable
 
+    if (!isRunning) {
+        isRunning = true;
+        timer = setInterval("incrementTimer()", 1000);
+    }
 }
 
 function incrementTimer() {
     // increment the timerTime
     // calculate the number of minutes and seconds
 
+    timerTime++;
+
     // write the minutes and seconds to the elements on the page
     // use the function pad to make sure there's a leading 0 if necessary
+
+    var min = Math.floor(timerTime / 60);
+    var sec = timerTime % 60;
+
+    document.getElementById('minutes').innerHTML = pad(min);
+    document.getElementById('seconds').innerHTML = pad(sec);
 }
 
 function pad(number) {
     // add a leading 0 if the number is < 10
+    if (number < 10) {
+        number = "0" + number;
+    }
+    return number;
 }
 
 function stopTimer() {
     // if the timer is running, stop it
-
+    if (isRunning) {
+        isRunning = false;
+        clearInterval(timer);
+    }
 }
 
 function resetTimer() {
     // stop the timer
-    
+    stopTimer();
     // set the timerTime back to 0
-    
+    timerTime = 0;
     // write 00 to the elements on the page for minutes and seconds
-
+    document.getElementById('minutes').innerHTML = '00';
+    document.getElementById('seconds').innerHTML = '00';
 }
 
 // When the page has finished loading, call the function init
+window.onload = init;
--- indexStart.js
+++ indexStart.js
@@ -13,8 +13,20 @@ class StopWatch
             event fires
             -- this.startButton.onclick = this.startTimer.bind(this);
     */
-    constructor() {
 
+    // constructs the StopWatch object
+    constructor() {
+        this.isRunning = false;
+        this.timer = null;
+        this.timerTime = 0;
+
+        let startBtn = document.getElementById("start");
+        let stopBtn = document.getElementById("stop");
+        let resetBtn = document.getElementById("reset");
+
+        startBtn.onclick = this.startTimer.bind(this);
+        stopBtn.onclick = this.stopTimer.bind(this);
+        resetBtn.onclick = this.resetTimer.bind(this);
     }
 
     /*
@@ -23,6 +35,56 @@ class StopWatch
         -   Remove the keyword function
         -   Add this. in front of every variable and method
     */
+
+    startTimer() {
+
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.timer = setInterval(this.incrementTimer.bind(this), 1000);
+        }
+    }
+
+    // moves the timer forward
+    incrementTimer() {
+
+        this.timerTime++;
+
+        // write the minutes and seconds to the elements on the page
+        // use the function pad to make sure there's a leading 0 if necessary
+
+        const min = Math.floor(this.timerTime / 60);
+        const sec = this.timerTime % 60;
+
+        document.getElementById('minutes').innerHTML = this.pad(min);
+        document.getElementById('seconds').innerHTML = this.pad(sec);
+    }
+
+    // adds leading 0 if number is less than 10
+    pad(number) {
+        // add a leading 0 if the number is < 10
+        if (number < 10) {
+            number = "0" + number;
+        }
+        return number;
+    }
+
+    // stops the timer if it is running
+    stopTimer() {
+        if (this.isRunning) {
+            this.isRunning = false;
+            clearInterval(this.timer.bind(this));
+        }
+    }
+
+    // resets the timer
+    resetTimer() {
+        this.stopTimer();
+
+        this.timerTime = 0;
+
+        document.getElementById('minutes').innerHTML = '00';
+        document.getElementById('seconds').innerHTML = '00';
+    }
 }
 
 // create a variable called stopWatch
